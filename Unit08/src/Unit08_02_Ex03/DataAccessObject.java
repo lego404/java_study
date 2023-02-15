9529666faa3469cce90d29f34fe480d7b322f2c6
Gone <71643183+lego404@users.noreply.github.com>
2023-02-16 01:00:59 +0900

    Add files via upload

---
+++ Unit08/src/Unit08_02_Ex03/DataAccessObject.java
@@ -0,0 +1,8 @@
+package Unit08_02_Ex03;
+
+public interface DataAccessObject {
+	public void select();
+	public void insert();
+	public void update();
+	public void delete();
+}
---
+++ Unit08/src/Unit08_02_Ex03/MySqlDao.java
@@ -0,0 +1,20 @@
+package Unit08_02_Ex03;
+
+public class MySqlDao implements DataAccessObject{
+	@Override
+	public void select() {
+		System.out.println("MySql에서 검색");
+	}
+	@Override
+	public void insert() {
+		System.out.println("MySql에 삽입");
+	}
+	@Override
+	public void update() {
+		System.out.println("MySql를 수정");
+	}
+	@Override
+	public void delete() {
+		System.out.println("MySql에서 삭제");
+	}
+}

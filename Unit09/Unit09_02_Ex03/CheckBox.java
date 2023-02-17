e170cd95731b3982d4be62810f90bc511573580e
Gone <71643183+lego404@users.noreply.github.com>
2023-02-18 04:30:08 +0900

    Add files via upload

---
+++ Unit09/Unit09_02_Ex03/CheckBox.java
@@ -0,0 +1,17 @@
+package Unit09_02_Ex03;
+
+public class CheckBox {
+	OnSelectListener listener;
+	
+	void setOnSelectListener(OnSelectListener listener) {
+		this.listener = listener;
+	}
+	
+	void select() {
+		listener.onSelect();
+	}
+	
+	static interface OnSelectListener{
+		void onSelect();
+	}
+}

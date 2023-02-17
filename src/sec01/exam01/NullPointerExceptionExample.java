3410201505ed086ed1b3fdf570eae8e546f8ce99
Gone <71643183+lego404@users.noreply.github.com>
2023-02-18 04:30:24 +0900

    Add files via upload

---
+++ src/sec01/exam01/NullPointerExceptionExample.java
@@ -0,0 +1,10 @@
+package sec01.exam01;
+/*
+ * NullPointerException
+ */
+public class NullPointerExceptionExample {
+	public static void main(String[] args) {
+		String data = null;
+		//System.out.println(data.toString());
+	}
+}

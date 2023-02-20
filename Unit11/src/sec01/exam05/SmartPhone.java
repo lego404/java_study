24c0cc4093dfd41a410d70572509a9d9e04fca40
Gone <71643183+lego404@users.noreply.github.com>
2023-02-21 02:11:03 +0900

    Add files via upload

---
+++ Unit11/src/sec01/exam05/SmartPhone.java
@@ -0,0 +1,18 @@
+package sec01.exam05;
+/*
+ * 객체의 문자 정보(toString() 메소드)
+ */
+public class SmartPhone {
+	private String company;
+	private String os;
+	
+	public SmartPhone(String company, String os) {
+		this.company = company;
+		this.os = os;
+	}
+	
+	@Override
+	public String toString() {	//toString 재정의
+		return company + ", " + os;
+	}
+}
